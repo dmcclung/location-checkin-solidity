cecb95d525c488087ed09f67fd56dddd76c43c62
dmcclung <35938+dmcclung@users.noreply.github.com>
2022-01-03 02:16:25 -0600

    Adding flag methods

--- contracts/Location.sol
+++ contracts/Location.sol
@@ -17,7 +17,7 @@ contract Location {
         uint256 points;
     }
 
-    struct Location {
+    struct LocationDescription {
         uint256 lat;
         uint256 lon;
         string name;
@@ -32,6 +32,11 @@ contract Location {
 
     // TODO: how to get all the users that share a location
     // and block number that is within a reasonable +/-
+    // Store a map of blocknumber to mapping of location id to users
+
+    // When someone switches their flag, an event is emitted, centralized
+    // process figures out which users need to be notified and updates
+    // the smart contract to record it
 
     using Counters for Counters.Counter;
     Counters.Counter private _locationIds;
@@ -47,6 +52,32 @@ contract Location {
     }
 
     function createLocation() public returns (uint256) {
-        
+        // Can anyone create a location? I think so. Just needs to be verified
+        // Hide non-verified in the UI
+    }
+
+    function setFlag() public {
+        // require user exists
+        // get user from map and set flag
+        // emit event
+    }
+
+    function checkIn(uint256 locationId) public {
+        // Create new userlocation with locationId
+        // TODO: Should caller be a checkIn trusted address
+    }
+
+    // TODO: Need to hide a location by verifier account
+
+    function verifyLocation(uint256 locationId) public {
+        // require that location exists
+        // require that verifier address made the call
+        // Set verified flag
+    }
+
+    function notifyUser(address user) public {
+        // get user of address, require it exists
+        // set flag of user
+        // TODO: Possibly set flag
     }
 }
