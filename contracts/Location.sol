5efa314fc2c8b0dbb255f826e0b50535d76912a6
dmcclung <35938+dmcclung@users.noreply.github.com>
2022-01-03 02:54:52 -0600

    Making progress on setup

--- contracts/Location.sol
+++ contracts/Location.sol
@@ -13,7 +13,6 @@ contract Location {
     
     struct User {
         bool flag;
-        UserLocation[] locations;
         uint256 points;
     }
 
@@ -24,50 +23,96 @@ contract Location {
         string description;
         string imageUrl;
         bool verified;
+        bool hide;
     }
 
     mapping(address => User) private userByAddress;
+    mapping(address => UserLocation[]) private locationsByUser;
+    mapping(uint256 => LocationDescription) private locationById;
 
-    mapping(uint256 => Location) private locationById;
-
-    // TODO: how to get all the users that share a location
+    // How to get all the users that share a location
     // and block number that is within a reasonable +/-
     // Store a map of blocknumber to mapping of location id to users
+    mapping(uint256 => mapping(uint256 => User[])) private usersByBlockNumber;
+
+    // TODO: Figure out how to notify users
+    // TODO: Update the above collection
+    // TODO: Write all events
 
     // When someone switches their flag, an event is emitted, centralized
     // process figures out which users need to be notified and updates
     // the smart contract to record it
+    event LocationCreated(uint256 locationId);
+    event UserCreated(address newUser);
+    event UserFlagSet(address user, bool flag);
+
+    address private verifier;
 
     using Counters for Counters.Counter;
     Counters.Counter private _locationIds;
     
     constructor() {
-        console.log("Deploying Location");
+        verifier = msg.sender;
+        console.log("Deploying Location, verifier %s", verifier);
     }
 
-    function getLocation(uint256 locationId) public view returns (Location memory) {
+    function getLocation(uint256 locationId) public view returns (LocationDescription memory) {
         require(locationId < _locationIds.current(), "Location does not exist");
-
         return locationById[locationId];
     }
 
-    function createLocation() public returns (uint256) {
+    function createLocation(uint256 lat, 
+                            uint256 lon, 
+                            string memory name, 
+                            string memory description,
+                            string memory imageUrl) public returns (uint256) {
+        // If created by verifier, set to true
+        bool isVerified = false;
+        // hide always false
+        bool isHidden = false;
         // Can anyone create a location? I think so. Just needs to be verified
         // Hide non-verified in the UI
+        LocationDescription memory location = LocationDescription(
+            lat,
+            lon,
+            name,
+            description,
+            imageUrl,
+            isVerified,
+            isHidden
+        );
+
+        uint256 newLocationId = _locationIds.current();
+        locationById[newLocationId] = location;
+        emit LocationCreated(newLocationId);
+        _locationIds.increment();
+        return newLocationId;
     }
 
-    function setFlag() public {
-        // require user exists
-        // get user from map and set flag
-        // emit event
+    function createUser() public {
+        uint256 defaultPoints = 1000;
+        bool flagged = false;
+        userByAddress[msg.sender] = User(flagged, defaultPoints);
+
+        emit UserCreated(msg.sender);
+    }
+
+    function setFlag(bool flag) public {
+        User memory user = userByAddress[msg.sender];
+        user.flag = flag;
+        
+        emit UserFlagSet(msg.sender, flag);
     }
 
     function checkIn(uint256 locationId) public {
         // Create new userlocation with locationId
-        // TODO: Should caller be a checkIn trusted address
     }
 
-    // TODO: Need to hide a location by verifier account
+    function hideLocation(uint256 locationId) public {
+        // require that verifier address made the call
+        // require that location exists
+        // set hide flag
+    }
 
     function verifyLocation(uint256 locationId) public {
         // require that location exists
@@ -77,7 +122,6 @@ contract Location {
 
     function notifyUser(address user) public {
         // get user of address, require it exists
-        // set flag of user
-        // TODO: Possibly set flag
+         
     }
 }
